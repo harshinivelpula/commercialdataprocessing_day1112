f8977b325751a141eaff6b97c63561743efa8f75
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-10-24 10:09:57 +0530

    add details

---
+++ customerinfo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace comercialdata4d11
+{
+    internal class customerinfo
+	{
+		"customerinfo":
+        
+		{
+			"customer_symbol": 1,
+			"customer_name": "Rishi",
+			"customer_shares": 50,
+			"customer_balance": 100000
+		},
+		{
+			"customer_symbol": 2,
+			"customer_name": "Abhi",
+			"customer_shares": 45,
+			"customer_balance": 99000
+		},
+		{
+	"customer_symbol": 3,
+			"customer_name": "Vishnu",
+			"customer_shares": 60,
+			"customer_balance": 111000
+		},
+		{
+	"customer_symbol": 4,
+			"customer_name": "charu",
+			"customer_shares": 30,
+			"customer_balance": 84000
+		},
+		{
+	"customer_symbol": 5,
+			"customer_name": "charugirl",
+			"customer_shares": 59,
+			"customer_balance": 105020
+		}
+
+	}
+}
